6eaa07575e1d2ff14dd05a8d4dc4ccc7c52fbefd
Sourabh <sraikwar@gmail.com>
2022-05-12 17:35:19 +0530

    [#1] ENHANCEMENT: Added SqlAlchemy in project

--- src/core/views.py
+++ src/core/views.py
@@ -1,9 +1,13 @@
+from re import U
 from app import app
 from flask import render_template, views
+from .models import User
+
 
 class RenderLandingPageView(views.View):
     
     def dispatch_request(self):
-        return render_template('pages/index.html')
+        users = User.query.all()
+        return render_template('pages/index.html', users=users)
 
 app.add_url_rule('/', view_func=RenderLandingPageView.as_view('landing_page'))
