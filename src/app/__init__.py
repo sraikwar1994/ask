fd292555f724d4b79ca0c0bbeb599c3344d148e9
Sourabh <sraikwar@gmail.com>
2022-05-13 11:15:07 +0530

    [#6] BUG: Added config file | updated .gitignore to exclude pycharm config files

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -4,10 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder="../templates")
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = "postgresql://flask_user:flask_password@localhost:5432/flask_db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config.from_pyfile("config.py")
 db = SQLAlchemy(app)
 
 import core.views
