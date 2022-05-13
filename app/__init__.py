4a1a90768ea1a0465fa424f7eba60b6305c0beb0
Sourabh <sraikwar@gmail.com>
2022-05-13 18:52:17 +0530

    [#3] ENHANCEMENT: added Migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os
-from flask import Flask, render_template
 
+from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="templates")
 app.config.from_object(os.environ.get("APP_SETTINGS", "config.DevelopmentConfig"))
@@ -8,12 +8,23 @@ app.config.from_object(os.environ.get("APP_SETTINGS", "config.DevelopmentConfig"
 
 def register_blueprints(application):
     from app.core import views
+
     application.register_blueprint(blueprint=views.bp)
+
+
+def migrate_db(application):
+    from flask_migrate import Migrate
+
+    from app.db import db
+
+    db.init_app(application)
+    Migrate(application, db)
 
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
 
 register_blueprints(app)
+migrate_db(app)
--- app/core/views.py
+++ app/core/views.py
@@ -1,12 +1,37 @@
-from flask import render_template, Blueprint
-from .models import User
+from flask import Blueprint, render_template
+from flask.views import MethodView, View
 
-bp = Blueprint('/', __name__, url_prefix='/')
+from app import app
 
-
-@bp.route(rule='/', methods=('GET',))
-def get():
-    users = User.query.all()
-    return render_template('pages/index.html', users=users)
+bp = Blueprint("/", __name__, url_prefix="/")
 
 
+class LandingPage(View):
+    def dispatch_request(self):
+        return render_template("pages/index.html")
+
+
+class UserAPI(MethodView):
+    def get(self, user_id):
+        if user_id is None:
+            # return a list of users
+            pass
+        else:
+            # expose a single user
+            pass
+
+    def post(self):
+        # create a new user
+        pass
+
+    def delete(self, user_id):
+        # delete a single user
+        pass
+
+    def put(self, user_id):
+        # update a single user
+        pass
+
+
+app.add_url_rule("/", view_func=LandingPage.as_view("landing_page"))
+app.add_url_rule("/users", view_func=LandingPage.as_view("user"))
